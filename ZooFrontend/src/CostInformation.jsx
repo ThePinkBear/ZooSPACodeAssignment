b7179c23910596531be2d526f760676835823b8e
ThePinkBear <bjorn.noctiluca@outlook.com>
2024-03-05 15:18:39 +0100

    Adjust landing page wording

--- ZooFrontend/src/CostInformation.jsx
+++ ZooFrontend/src/CostInformation.jsx
@@ -6,8 +6,8 @@ function CostInformation({cost, meatCost, fruitCost}) {
       <h2>Zoo Feeding App</h2>
       <h3>Current Cost of feeding the animals:</h3>
       <h2>${cost} per day.</h2>
-      <h3>Cost of feeding the animals:</h3>
-      <h3>${meatCost} on meat, ${fruitCost} on fruit</h3>
+      <h3>Cost by food source:</h3>
+      <h3>Meat: ${meatCost}, Fruit ${fruitCost}</h3>
     </>
   )
 }
