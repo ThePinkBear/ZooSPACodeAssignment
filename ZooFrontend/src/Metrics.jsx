c15e97a1ec8f1e72478ebecb5871a4fb23852b4d
ThePinkBear <bjorn.noctiluca@outlook.com>
2024-03-06 12:54:07 +0100

    Add pie chart for diets

--- ZooFrontend/src/Metrics.jsx
+++ ZooFrontend/src/Metrics.jsx
@@ -5,15 +5,26 @@ import './Metrics.css';
 
 function Metrics ({ animals }) {
   const [animalData, setAnimalData] = useState([["Name", "Weight"]]);
+  const [dietData, setDietData] = useState([["Diet", "Count"]]);
 
   const populateAnimalsByWeight = () => {
     const header = [["Name", "Kilos"]];
     const mappedAnimals = animals.map(animal => [animal.name, animal.weight]);
     setAnimalData(header.concat(mappedAnimals));
   }
+  const populateDietData = () => {
+    const dietCounts = animals.reduce((acc, animal) => {
+      acc[animal.diet] = (acc[animal.diet] || 0) + 1;
+      return acc;
+    }, {});
+
+    const mappedDietData = Object.entries(dietCounts).map(([diet, count]) => [diet, count]);
+    setDietData([["Diet", "Count"]].concat(mappedDietData));
+  }
 
   useEffect(() => {
     populateAnimalsByWeight();
+    populateDietData();
   }, [animals]);
 
   return (
@@ -27,6 +38,18 @@ function Metrics ({ animals }) {
           width= "90%"
           height="50%"
         />
+        </article>
+        <article className='chartArticle'>
+        <h4>Animal Diet Ratio:</h4>
+        <Chart
+        chartType="PieChart"
+        data={dietData}
+        width="90%"
+        height="50%"
+        options={{
+          is3D: true,
+        }}
+      />
       </article>
     </section>
     </>
